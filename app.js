a361a7ab3fcd8e5328e704d2c235e0875eb13e94
Miguel-Lopez-97 <miguel.m.lopz.p@gmail.com>
2021-12-13 15:03:26 -0500

    Simplificacion del JS funciones de dibujo

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ let blocks = [
     [2,0,3,5,4],
     [1,0,5,6,3]] */
 
-newBlock = Math.floor(Math.random()*3)
-    console.log(newBlock)
+
 
 let newBlocks = function () {
     this.x =5
@@ -52,73 +51,94 @@ let newBlocks = function () {
     this.rot = false
     this.l
     this.n
+    
+    this.aleatorio = function(){
+        this.newBlock = Math.floor(Math.random()*1)
+        console.log(newBlock)
+        return this.newBlock}
+    
+    
+
+
+// Matriz de transformación
+
 
     this.rotate = function () {
         if(this.rot==true){this.rot=false}
         else{this.rot=true}
         console.log(this.rot)
         return this.rot
+        
     }
 
     this.draw = function(){
-        
-        switch(newBlock){
+
+
+        switch(this.newBlock){
 
             case 0:
-                if(this.rot==true){
                     l=1
                     n=4
                     for (i=0;i <n ; i++){
-                        for (j=0;j <l ; j++){
-                            scenary[this.y+i][this.x+j] = 1             
+                        for (j=0;j <l ; j++){     
+                            ctx.fillStyle   =   block
+                            ctx.fillRect(   (this.x+j)*widthC,  (this.y+i)*heightC,  widthC, heightC)
                         }          
                     }
-                }    
-                
+                  
+               /*  
                 if(this.rot==false){
                     l=4
                     n=1
                     for (i=0;i <n ; i++){
                         for (j=0;j <l ; j++){
-                            scenary[this.y+i][this.x+j] = 1             
+                            ctx.fillStyle   =   block
+                            ctx.fillRect(   (this.x+j)*widthC,  (this.y+i)*heightC,  widthC, heightC)           
                         }          
                     }   
-                }
+                } */
             break
-            case 1:
+            
+            /* case 1:
                 l=2
                 n=2
                 for (i=0;i <n ; i++){
                     for (j=0;j <l ; j++){
-                        scenary[this.y+i][this.x+j] = 1             
+                        ctx.fillStyle   =   block
+                        ctx.fillRect(   (this.x+j)*widthC,  (this.y+i)*heightC,  widthC, heightC)            
                     }          
                 }
             break
+            
             case 2:
                 if(this.rot==true){
                     l=3
                     n=2
                     for (i=0;i <1 ; i++){
                         for (j=0;j <l ; j++){
-                            scenary[this.y+i][this.x+j] = 1             
+                            ctx.fillStyle   =   block
+                            ctx.fillRect(   (this.x+j)*widthC,  (this.y+i)*heightC,  widthC, heightC)           
                         }
                     }
                     for (i=1;i <n ; i++){
-                            scenary[this.y+i][this.x+l-1] = 1             
+                        ctx.fillStyle   =   block
+                        ctx.fillRect(   (this.x+j-1)*widthC,  (this.y+i)*heightC,  widthC, heightC)            
                     }
                 }
                 if(this.rot==false){
                     l=2
                     n=3
                     for (i=0;i <1 ; i++){
-                        for (j=0;j <l ; j++){
-                            scenary[this.y+i][this.x+j] = 1             
+                        for (j=0;j <l ; j++){ 
+                            ctx.fillStyle   =   block
+                        ctx.fillRect(   (this.x+j)*widthC,  (this.y+i)*heightC,  widthC, heightC)          
                         }
                     }
-                    for (i=1;i <n ; i++){
-                            scenary[this.y+i][this.x] = 1             
+                    for (i=1;i <n ; i++){  
+                            ctx.fillStyle   =   block
+                        ctx.fillRect(   (this.x)*widthC,  (this.y+i)*heightC,  widthC, heightC)          
                     }
-                }
+                } */
                 /* if(this.rot==false){
                     l=2
                     n=3
@@ -142,9 +162,9 @@ let newBlocks = function () {
                             scenary[this.y+i][this.x+j] = 1             
                         }
                     }
-                }  */          
+                }    */       
                 
-            break
+            break 
 
         }
     }
@@ -159,18 +179,28 @@ let newBlocks = function () {
     this.down = function(){
         if(this.y+n<scenary.length && this.margins(this.y+n,this.x+l) == false)
         {this.y++
-        for(i=0; i<l; i++)
-        {scenary[this.y-1][this.x+i] = 0}
+        
         }
-        else{this.y}
+        else{this.y
+            for (i=0;i <n ; i++){
+                    for (j=0;j <l ; j++){
+                        scenary[this.y+i][this.x+j] = 1  
+                    }
+            }
+            this.aleatorio()
+            this.draw()
+            this.x =5
+            this.y = 0
+            }
     }
-   
+
+    
     this.left = function(){
         if(this.x-1>=0 && this.margins(this.y,this.x-1) == false)
         {   this.x--
-            for(i=0; i<n; i++)
+            /* for(i=0; i<n; i++)
             {   scenary[this.y+i][this.x+1] = 0
-                scenary[this.y+i][this.x+l] = 0}
+                scenary[this.y+i][this.x+l] = 0} */
         }
         else{this.x}
         
@@ -179,9 +209,9 @@ let newBlocks = function () {
     this.right = function(){     
         if(this.x+l<scenary[this.y].length && this.margins(this.y,this.x+l) == false)
         {   this.x++
-            for(i=0; i<n; i++)
-            {   scenary[this.y+i][this.x-1] = 0
-                /* scenary[this.y+i][this.x+l] = 0 */}
+           /*  for(i=0; i<n; i++)
+            {   scenary[this.y+i][this.x-1] = 0 */
+                /* scenary[this.y+i][this.x+l] = 0 }*/
         }
         else{this.x}
         
@@ -226,6 +256,7 @@ function init(){
     ctx=canvas.getContext("2d")
     
     player = new newBlocks()
+    player.aleatorio()
 
     document.addEventListener("keydown", function(key){
         if(key.key  ==  "ArrowDown" || key.key  ==  "s"){player.down()}
---
+++ script.js
@@ -0,0 +1,261 @@
+//Variables generales del juego
+
+let canvas;
+let ctx;
+let newBlock;
+
+    //FPS
+const FPS = 60;
+
+    //Colors
+let block = "white"
+let fondo ="#007D5F"
+
+//Scenary Array
+
+let columns = 15;
+let rows = 20;
+
+let scenary = new Array(rows)
+    for (let n=0; n<rows;n++){
+        scenary[n]=new Array(columns)
+            for( let m=0; m<columns; m++){
+                scenary[n][m]=0;
+            }
+    }
+    //Size of Cube
+
+let widthC = 25;
+let heightC = 25;
+
+//Blocks Array
+
+let newBlocks = function () {
+
+    this.x =5
+    this.y = 0
+    //Rotacion
+    this.rot = Math.ceil(Math.random()*4)
+    //Largo de la pieza
+    this.l
+    //Ancho de la pieza
+    this.n
+
+    this.aleatorio = function(){
+        this.newBlock = Math.floor(Math.random()*1)
+        console.log(this.newBlock)
+        return this.newBlock
+    }
+
+    this.rotate = function () {
+
+        if(this.rot>=0 &&this.rot<4){this.rot++}
+        else{this.rot=1}
+        console.log(this.rot)
+        return this.rot}
+
+    this.draw = function(){
+
+        this.drawBlock = function(a,c,b,d){
+            for (i=c;i <a ; i++){
+                for (j=d;j <b ; j++){
+                    ctx.fillStyle   =   block
+                    ctx.fillRect(   (this.x+j)*widthC,  (this.y+i)*heightC,  widthC, heightC)
+                }
+            }
+        }
+        
+        switch(this.newBlock){
+            //linea
+            case 1:
+                if(this.rot==1 || this.rot==3 ){this.drawBlock(1,0,4,0)
+                l=4
+                n=1
+                }
+                
+                else if(this.rot==2 || this.rot==4 ){this.drawBlock(4,0,1,0)
+                l=1
+                n=4 
+                }
+            break
+            //Cuadrado
+            case 2:
+                if(this.rot==1) {this.drawBlock(2,0,2,0)
+                l=2
+                n=2}
+            break
+            //L 
+            case 3:
+                if(this.rot==1) {this.drawBlock(1,0,3,0)
+                    this.drawBlock(2,1,3,2)
+                    l=3
+                    n=2}
+                else if(this.rot==2) {
+                    this.drawBlock(1,0,2,1)
+                    this.drawBlock(2,1,2,1)
+                    this.drawBlock(3,2,2,0)
+                    l=2
+                    n=3}
+                else if(this.rot==3) {
+                    this.drawBlock(1,0,1,0)
+                    this.drawBlock(2,1,3,0)
+                    l=3
+                    n=2}   
+                else if(this.rot==4) {
+                    this.drawBlock(1,0,2,0)
+                    this.drawBlock(2,1,1,0)
+                    this.drawBlock(3,2,1,0)
+                    l=2
+                    n=3} 
+            break
+            //L invertida
+            case 4:
+                if(this.rot==1) {
+                    this.drawBlock(1,0,3,0)
+                    this.drawBlock(2,1,1,0)
+                    l=3
+                    n=2}
+                else if(this.rot==2) {
+                    this.drawBlock(1,0,2,0)
+                    this.drawBlock(2,1,2,1)
+                    this.drawBlock(3,2,2,1)
+                    l=2
+                    n=3}
+                else if(this.rot==3) {
+                    this.drawBlock(1,0,3,2)
+                    this.drawBlock(2,1,3,0)
+                    l=3
+                    n=2}
+                else if(this.rot==4) {
+                    this.drawBlock(1,0,1,0)
+                    this.drawBlock(2,1,1,0)
+                    this.drawBlock(3,2,2,0)
+                    l=2
+                    n=3}
+            break
+            //T
+            case 5:
+                if(this.rot==1) {
+                    this.drawBlock(1,0,2,1)
+                    this.drawBlock(2,1,3,0)
+                    l=3
+                    n=2}
+                else if(this.rot==2) {
+                    this.drawBlock(1,0,1,0)
+                    this.drawBlock(2,1,2,0)
+                    this.drawBlock(3,2,1,0)
+                    l=2
+                    n=3}
+                else if(this.rot==3) {
+                    this.drawBlock(1,0,3,0)
+                    this.drawBlock(2,1,2,1)
+                    l=3
+                    n=2}
+                else if(this.rot==4) {
+                    this.drawBlock(1,0,2,1)
+                    this.drawBlock(2,1,2,0)
+                    this.drawBlock(3,2,2,1)
+                    l=2
+                    n=3}
+            break
+            //Z
+            case 6:
+                if(this.rot==1 || this.rot==3) {
+                    this.drawBlock(1,0,2,0)
+                    this.drawBlock(2,1,3,1)
+                l=3
+                n=2}
+                else if(this.rot==2 || this.rot ==4) {
+                    this.drawBlock(1,0,2,1)
+                    this.drawBlock(2,1,2,0)
+                    this.drawBlock(3,2,1,0)
+                    l=2
+                    n=3}
+            break
+            //Z invertida
+            case 0:
+                if(this.rot==1|| this.rot==3) {
+                    this.drawBlock(1,0,3,1)
+                    this.drawBlock(2,1,2,0)
+                l=3
+                n=2}
+                else if(this.rot==2|| this.rot==4) {
+                    this.drawBlock(1,0,1,0)
+                    this.drawBlock(2,1,2,0)
+                    this.drawBlock(3,2,2,1)
+                    l=2
+                    n=3}
+            break
+        }
+
+    }
+
+    this.margins = function(y,x){
+        let colision = true
+        if (scenary[y][x] == 0)
+            {colision   = false}
+        return colision
+    }
+
+    this.down = function(){
+        if(this.y+n<scenary.length)
+        {this.y++}
+        else{this.y}
+    }
+
+    this.left = function(){
+        if(this.x-1>=0)
+        {   this.x--}
+        else{this.x}       
+    }
+
+    this.right = function(){     
+        if(this.x+l<scenary[this.y].length)
+        {   this.x++}
+        else{this.x}     
+    }
+}
+
+function drawScenary() {
+    let color;
+
+    for(    y=0;    y<scenary.length;   y++){
+        for(    x=0;    x<scenary[y].length;    x++){
+                if( scenary[y][x] ==    0){color    =   fondo}
+                if( scenary[y][x] ==    1){color    =   block}
+                ctx.fillStyle   =   color
+                ctx.fillRect(   x*widthC,  y*heightC,  widthC, heightC)
+                ctx.strokeStyle = "white"
+                ctx.strokeRect(   x*widthC,  y*heightC,  widthC, heightC)
+            }
+    }
+   
+}
+
+let player;
+
+function init(){
+    canvas=document.getElementById("canva")
+    ctx=canvas.getContext("2d")
+    
+    player = new newBlocks()
+    player.aleatorio()
+
+    document.addEventListener("keydown", function(key){
+        if(key.key  ==  "ArrowDown" || key.key  ==  "s"){player.down()}
+        if(key.key  ==  "ArrowLeft" || key.key  ==  "a"){player.left()}
+        if(key.key  ==  "ArrowRight"|| key.key  ==  "d"){player.right()}
+        if(key.key  ==  "ArrowUp"|| key.key  ==  "w"){player.rotate()}
+    })
+
+    setInterval(
+        function () {principal()},
+        1000/FPS)
+    
+}
+
+function principal(){
+    drawScenary()
+    player.draw()
+
+}
